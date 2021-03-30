f51689807d3cd23dbe5fc2cac2100a7d7f4e4ef7
pengoodsoft <majoron@qq.com>
2021-03-30 10:35:19 +0800

    init repo

---
+++ src/test/java/com/majcms/community/MajcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majcms.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
